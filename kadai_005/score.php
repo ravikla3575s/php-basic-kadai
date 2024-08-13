8d3b993d5fa8de04798bbae0b06839e04a230977
ravi3575s <122154104+ravikla3575s@users.noreply.github.com>
2024-08-13 22:36:06 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,9 +1,21 @@
 <?php
+$input = '80,60,55,40,100,25,80,95,30,60';
+$scores = explode(",", $input);
+foreach ($scores as $index => $score) {
+    ${'score' . ($index + 1)} = $score;
+    echo 'score' . ($index + 1) . ': ' . ${'score' . ($index + 1)} . "\n";
+};
+
 function sum($carry, $item)
 {
     $carry += $item;
     return $carry;
 }
-$score = array(80,60,55,40,100,25,80,95,30,60);
-echo array_reduce($score, "sum")/count($score);
+
+// $scores を再設定しない
+$totalScore = array_reduce($scores, "sum");
+$averageScore = $totalScore / count($scores);
+
+echo "合計: " . $totalScore . "\n";
+echo "平均: " . $averageScore . "\n";
 ?>
