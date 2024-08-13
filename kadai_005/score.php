3b3e04c381a31aee0eca5d3e5ae550033a3028ab
ravi3575s <122154104+ravikla3575s@users.noreply.github.com>
2024-08-13 23:05:03 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -5,13 +5,13 @@ $scores = explode(",", $input);
 $totalScore = 0;
 for ($i = 0; $i < count($scores); $i++) {
     ${'score' . ($i + 1)} = $scores[$i];
-    echo 'score' . ($i + 1) . ': ' . ${'score' . ($i + 1)} . "\n";
+    echo 'score' . ($i + 1) . ': ' . ${'score' . ($i + 1)} . "<br>";
     $totalScore += ${'score' . ($i + 1)};
 }
-
+echo $i.'<br>';
 $averageScore = $totalScore / $i;
-echo "合計: " . $totalScore . "\n";
-echo "平均: " . $averageScore . "\n";
+echo "合計: " . $totalScore . "<br>";
+echo "平均: " . $averageScore . "<br>";
 ?>
 
 <!-- <?php
@@ -19,7 +19,7 @@ $input = '80,60,55,40,100,25,80,95,30,60';
 $scores = explode(",", $input);
 foreach ($scores as $index => $score) {
     ${'score' . ($index + 1)} = $score;
-    echo 'score' . ($index + 1) . ': ' . ${'score' . ($index + 1)} . "\n";
+    echo 'score' . ($index + 1) . ': ' . ${'score' . ($index + 1)} . "<br>";
 };
 
 function sum($carry, $item)
@@ -31,6 +31,6 @@ function sum($carry, $item)
 $totalScore = array_reduce($scores, "sum");
 $averageScore = $totalScore / count($scores);
 
-echo "合計: " . $totalScore . "\n";
-echo "平均: " . $averageScore . "\n";
+echo "合計: " . $totalScore . "<br>";
+echo "平均: " . $averageScore . "<br>";
 ?> -->
