f22ec9e7533169450cb1bf4aea3eeca441a8368e
ravi3575s <122154104+ravikla3575s@users.noreply.github.com>
2024-08-13 22:47:10 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,6 +1,22 @@
 <?php
 $input = '80,60,55,40,100,25,80,95,30,60';
 $scores = explode(",", $input);
+
+$totalScore = 0;
+for ($i = 0; $i < count($scores); $i++) {
+    ${'score' . ($i + 1)} = $scores[$i];
+    echo 'score' . ($i + 1) . ': ' . ${'score' . ($i + 1)} . "\n";
+    $totalScore += ${'score' . ($i + 1)};
+}
+
+$averageScore = $totalScore / $i;
+echo "合計: " . $totalScore . "\n";
+echo "平均: " . $averageScore . "\n";
+?>
+
+<!-- <?php
+$input = '80,60,55,40,100,25,80,95,30,60';
+$scores = explode(",", $input);
 foreach ($scores as $index => $score) {
     ${'score' . ($index + 1)} = $score;
     echo 'score' . ($index + 1) . ': ' . ${'score' . ($index + 1)} . "\n";
@@ -12,10 +28,9 @@ function sum($carry, $item)
     return $carry;
 }
 
-// $scores を再設定しない
 $totalScore = array_reduce($scores, "sum");
 $averageScore = $totalScore / count($scores);
 
 echo "合計: " . $totalScore . "\n";
 echo "平均: " . $averageScore . "\n";
-?>
+?> -->
