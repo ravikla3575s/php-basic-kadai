975def9a3d2335948cc47d691e30f5a3e7e465d8
ravi3575s <122154104+ravikla3575s@users.noreply.github.com>
2024-08-15 03:47:06 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,42 @@
+<?php
+class Food {
+    private $name;
+    private $price;
+
+    public function __construct(string $name, int $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function show_price() {
+        echo $this->price . '<br>';
+    }
+}
+
+class Animal {
+    private $name;
+    private $height;
+    private $weight;
+
+    public function __construct(string $name, int $height, int $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+    }
+
+    public function show_weight() {
+        echo $this->weight . '<br>';
+    }
+}
+
+$potato = new Food('potato', 250);
+$dog = new Animal('dog', 60, 5000);
+
+print_r($potato);
+echo '<br>';
+print_r($dog);
+echo '<br>';
+
+$potato->show_price();
+$dog->show_weight();
+?>
